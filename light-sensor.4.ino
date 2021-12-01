e669f199677471eec8a51aba5d80401f42e23efc
Suzanne Michaela Aasheim-Sørensen <92297087+SuzanneMichaella@users.noreply.github.com>
2021-12-01 17:20:21 +0100

    loop with the light and motor connected
    just a running loop where the motor is changing only based on the light level.

---
+++ light-sensor.4.ino
@@ -0,0 +1,33 @@
+#include <Servo.h>
+
+const int servoPin = 9;
+Servo myServo;
+const int LDR = A0;
+int input_val = 0;
+
+
+void setup()
+{
+  Serial.begin(9600);
+  myServo.attach (servoPin); 
+
+}
+
+void loop()
+{
+  input_val = analogRead(LDR);
+  Serial.print("LDR Value is: ");
+  Serial.println(input_val);
+  delay(1000);
+
+if (input_val > 25){
+  myServo.write(180);
+  Serial.println(180);
+  delay (2000);
+}
+else {
+  myServo.write(0);
+  Serial.println(0);
+  delay (2000);
+}
+}
